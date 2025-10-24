85a35fede5440c2271394293daafbdf03c98050d
qujunhong-ops <2179272405@qq.com>
2025-10-24 20:24:00 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,11 +2,17 @@
 //2179272405@qq.com
 //屈军宏
 #include<stdio.h>;
+
 int main()
 {
-    float temperature = 0;    
-printf("请输入摄氏度：");    
-scanf("%f",&temperature);
+
+    float temperature = 0;
+
+    printf("请输入摄氏度：");
+    scanf("%f",&temperature);
+
     printf("华氏度为：%.1f",temperature * 1.8 + 32);
+
     return 0;
+
 }
