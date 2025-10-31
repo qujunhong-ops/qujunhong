84df1343803f82dc2a57a72731c533605aaa7b68
qujunhong-ops <2179272405@qq.com>
2025-10-31 15:11:53 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -5,28 +5,20 @@
 
 int main()
 {
-    int grade;
-
-    printf("请输入学生成绩：");
-    scanf("%d",&grade);
-
-if (grade > 100 || grade < 0){
-    printf("请输入正确的成绩");
-}
-else if(grade >= 90 )
-{
-    printf("A");
-}
-else if (grade >= 80 ){
-    printf("B");
-}
-else if(grade >= 70 ){
-    printf("C");
-}
-else if(grade >= 60 ){
-    printf("D");
-}else if(grade >= 0 ){
-    printf("E");
-}
+    int i,j,k;
+    int num;
+    char separator;
+    for ( num = 100; num < 500; num += 1)
+    {
+        i = num / 100;
+        j = (num % 100)/10;
+        k = num % 10;
+    
+    if(num == i*i*i + j*j*j + k*k*k)
+    {
+        printf("%c%d",separator,num);
+         separator =' ';
+    }
+    }
 return 0;
-} 
+}
