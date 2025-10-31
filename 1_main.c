4429616205c25cc943c0cdb162646ecd854488c6
qujunhong-ops <2179272405@qq.com>
2025-10-31 15:09:44 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,18 +1,36 @@
 //202510304205
 //2179272405@qq.com
 //屈军宏
-#include<stdio.h>;
+#include<stdio.h>
 
 int main()
 {
-
-    float temperature = 0;
-
-    printf("请输入摄氏度：");
-    scanf("%f",&temperature);
-
-    printf("华氏度为：%.1f",temperature * 1.8 + 32);
-
+    int num;
+    int i;
+    printf("请输入一个50以内的正整数");
+    scanf("%d",&num);
+    if (num <= 1)
+    {
+        printf("密钥不安全，请重新输入");
+        return 1;
+    }
+    if (num == 2)
+    {
+        printf("密钥安全，密码设置成功");
+        return 1;
+    }
+    for ( i = 3; i < 50; i += 1)
+    {
+        if(num % i == 0){
+            break;
+        }
+    }
+    if(i == num){
+        printf("密钥安全，密码设置成功");
+        return 1;
+    }else{
+        printf("密钥不安全，请重新输入");
+        return 1;
+    }
     return 0;
-
 }
